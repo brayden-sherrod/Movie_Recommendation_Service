447245662207b1f3997a0e88fc57dec353f9d75f
Rose Soriano <laurensori@tamu.edu>
2021-09-29 11:13:09 -0500

    Added edits

--- module1/src/remove_column.java
+++ module1/src/remove_column.java
@@ -1,13 +1,13 @@
 import java.io.*;
 import java.util.Scanner;
 
-public class population_script {
+public class remove_column {
     public static void main(String[] args) throws Exception {
         //parsing a CSV file into Scanner class constructor
         Scanner sc1 = new Scanner(new File("../../data/crew.csv"));
         sc1.useDelimiter("\n");   //sets the delimiter pattern
         while (sc1.hasNext()) {  //returns a boolean value
-            System.out.print(sc1.next());  //find and returns the next complete token from this scanner
+            System.out.println(sc1.next());  //find and returns the next complete token from this scanner
         }
         sc1.close();  //closes the scanner
     }
