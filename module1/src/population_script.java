1d68f99806389c9abcba3df2c492ad22817d2066
amcgrath17 <amcgrath17@tamu.edu>
2021-09-29 10:55:47 -0500

    Fixed small Java error

--- module1/src/population_script.java
+++ module1/src/population_script.java
@@ -5,7 +5,7 @@ public class population_script {
     public static void main(String[] args) throws Exception {
         //parsing a CSV file into Scanner class constructor
         Scanner sc1 = new Scanner(new File("data/crew.csv"));
-        sc.useDelimiter(",");   //sets the delimiter pattern
+        sc1.useDelimiter(",");   //sets the delimiter pattern
         while (sc1.hasNext()) {  //returns a boolean value
             System.out.print(sc1.next());  //find and returns the next complete token from this scanner
         }
