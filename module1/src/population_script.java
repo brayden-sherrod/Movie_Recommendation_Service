a50046fba4c9075a91ff1d1f3fbf6c755c0875ee
amcgrath17 <amcgrath17@tamu.edu>
2021-09-29 11:02:26 -0500

    Fixed file path

--- module1/src/population_script.java
+++ module1/src/population_script.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class population_script {
     public static void main(String[] args) throws Exception {
         //parsing a CSV file into Scanner class constructor
-        Scanner sc1 = new Scanner(new File("data/crew.csv"));
+        Scanner sc1 = new Scanner(new File("../../data/crew.csv"));
         sc1.useDelimiter(",");   //sets the delimiter pattern
         while (sc1.hasNext()) {  //returns a boolean value
             System.out.print(sc1.next());  //find and returns the next complete token from this scanner
