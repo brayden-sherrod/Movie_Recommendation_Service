ae9d87b9cb415de23c0e61835938bdec5c0c4877
Rose Soriano <rosesoriano.dtx@gmail.com>
2021-09-30 17:27:43 -0500

    Last stuff

--- module1/src/population_script.java
+++ module1/src/population_script.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.Scanner;
 
 public class population_script {
-    
+
     /*----------------------------------------ONE FOR EACH CLEANED CSV---------------------------------------*/
 
     public static void scanCrewMembers() {
@@ -10,35 +10,36 @@ public class population_script {
     }
 
     public static void scanCustomersRatings() {
-        
+
     }
 
     public static void scanCustomesrWatchedLists() {
-        
+
     }
 
     public static void scanMediaCollection() {
-        
+
     }
 
     public static void scanMediaCrewMembers() {
-        
+
     }
 
     public static void scanMediaGenres() {
         // string s;
-        // String[] arrays = s.split(",", 2); 
+        // String[] arrays = s.split(",", 2);
     }
 
     /*-----------------------------------------------------------------------------------------------------*/
 
     /**
      * For a column category, it splits up possible multiple entries
+     * 
      * @param pod - String of comma separated elements
      */
     public static void separatePod(String pod) {
         String[] splitPod = pod.split(",");
-        
+
         // Iterate over array
         for (String element : splitPod) {
             System.out.print(element + "-");
@@ -47,18 +48,19 @@ public class population_script {
 
     /**
      * Parses through a csv file provided.
+     * 
      * @param fileName - Name of csv file
      * @throws FileNotFoundException
      */
     public static void scanFile(String fileName) throws FileNotFoundException {
         Scanner sc1 = new Scanner(new File(fileName));
-        sc1.useDelimiter("\n");   // Sets the delimiter pattern
+        sc1.useDelimiter("\n"); // Sets the delimiter pattern
 
         System.out.println(sc1.next()); // Skips first line
 
-        while (sc1.hasNext()) {  // Returns a boolean value
-            String[] splitLine = (sc1.next()).split(",");   // Splits each line into array
-            
+        while (sc1.hasNext()) { // Returns a boolean value
+            String[] splitLine = (sc1.next()).split(","); // Splits each line into array
+
             // Iterate over array
             for (String pod : splitLine) {
                 separatePod(pod);
@@ -66,23 +68,18 @@ public class population_script {
             }
             System.out.println();
         }
-        sc1.close();  // Closes the scanner
+        sc1.close(); // Closes the scanner
     }
 
     public static void main(String[] args) throws FileNotFoundException {
-        //* cleansing
-        //TODO
+        // * cleansing
+        // TODO
         // keep most recent media if there are duplicates
         // redirect output to a file in folder clean_data
-        scanFile("../../cleanedCSVFIles/crew_member.csv"); //? starts at 6405819
-        //scanFile("../../data/customer_ratings.csv");
-        //scanFile("../../data/names.csv");
-        //scanFile("../../data/principals.csv");
-        //scanFile("../../data/titles.csv");
+        scanFile("../../cleanedCSVFIles/crew_member.csv"); // ? starts at 6405819
+        // scanFile("../../data/customer_ratings.csv");
+        // scanFile("../../data/names.csv");
+        // scanFile("../../data/principals.csv");
+        // scanFile("../../data/titles.csv");
     }
 }
-
-
-
-
-
