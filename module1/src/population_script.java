0d6585c01ed657bfdfab0661cce9c23d2b6c5982
Rose Soriano <laurensori@tamu.edu>
2021-09-29 23:45:13 -0500

    Split file by line, then added split function

--- module1/src/population_script.java
+++ module1/src/population_script.java
@@ -2,13 +2,27 @@ import java.io.*;
 import java.util.Scanner;
 
 public class population_script {
-    public static void main(String[] args) throws Exception {
-        //parsing a CSV file into Scanner class constructor
+
+    /**
+     * Parses through a csv file provided.
+     * @param fileName Name of csv file
+     * @throws FileNotFoundException
+     */
+    public static void scanFile(String fileName) throws FileNotFoundException {
         Scanner sc1 = new Scanner(new File("../../data/crew.csv"));
-        sc1.useDelimiter(",");   //sets the delimiter pattern
+        sc1.useDelimiter("\n");   //sets the delimiter pattern
         while (sc1.hasNext()) {  //returns a boolean value
-            System.out.print(sc1.next());  //find and returns the next complete token from this scanner
+            String[] splitLine = (sc1.next()).split("\t", 2);   // Splits each line into array
+            System.out.println(splitLine[0]);                   // Only print first unneccesary column
         }
         sc1.close();  //closes the scanner
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        scanFile("../../data/crew.csv");
+        scanFile("../../data/customer_ratings.csv");
+        scanFile("../../data/names.csv");
+        scanFile("../../data/principals.csv");
+        scanFile("../../data/titles.csv");
     }
 }
