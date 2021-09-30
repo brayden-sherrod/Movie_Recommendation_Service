779734a8ce8c2f6d329bf48083862a0981fc40e9
Rose Soriano <laurensori@tamu.edu>
2021-09-30 00:20:59 -0500

    Successfully can get each element

--- module1/src/population_script.java
+++ module1/src/population_script.java
@@ -2,6 +2,19 @@ import java.io.*;
 import java.util.Scanner;
 
 public class population_script {
+    
+    /**
+     * For a column category, it splits up possible multiple entries
+     * @param pod 
+     */
+    public static void separatePod(String pod) {
+        String[] splitPod = pod.split(",");
+        
+        // Iterate over array
+        for (String element : splitPod) {
+            System.out.print(element + "-");
+        }
+    }
 
     /**
      * Parses through a csv file provided.
@@ -10,19 +23,25 @@ public class population_script {
      */
     public static void scanFile(String fileName) throws FileNotFoundException {
         Scanner sc1 = new Scanner(new File("../../data/crew.csv"));
-        sc1.useDelimiter("\n");   //sets the delimiter pattern
-        while (sc1.hasNext()) {  //returns a boolean value
-            String[] splitLine = (sc1.next()).split("\t", 2);   // Splits each line into array
-            System.out.println(splitLine[0]);                   // Only print first unneccesary column
+        sc1.useDelimiter("\n");   // Sets the delimiter pattern
+        while (sc1.hasNext()) {  // Returns a boolean value
+            String[] splitLine = (sc1.next()).split("\t");   // Splits each line into array
+            
+            // Iterate over array
+            for (String pod : splitLine) {
+                separatePod(pod);
+                System.out.print("|");
+            }
+            System.out.println();
         }
-        sc1.close();  //closes the scanner
+        sc1.close();  // Closes the scanner
     }
 
     public static void main(String[] args) throws FileNotFoundException {
         scanFile("../../data/crew.csv");
-        scanFile("../../data/customer_ratings.csv");
-        scanFile("../../data/names.csv");
-        scanFile("../../data/principals.csv");
-        scanFile("../../data/titles.csv");
+        // scanFile("../../data/customer_ratings.csv");
+        // scanFile("../../data/names.csv");
+        // scanFile("../../data/principals.csv");
+        // scanFile("../../data/titles.csv");
     }
 }
