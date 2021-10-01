99fa706020d8a5511cdd682fa78a4f172ed1fc44
Rose Soriano <rosesoriano.dtx@gmail.com>
2021-10-01 14:31:04 -0500

    Removed extra code we havent thought of yet

--- module1/src/population_script.java
+++ module1/src/population_script.java
@@ -166,61 +166,68 @@ public class population_script {
 
     // * DONT need to iterate a primary key
     public static void scanMediaCollection(Connection conn) {
+        // Populate Database
+        try{
+            System.out.println("Populating CrewMembers Table...");
+            
+            String fileName = "../../cleanedCSVFiles/crew_member.csv";
+            
+            Scanner sc = new Scanner(new File(fileName));
+            sc.useDelimiter("\n"); // Sets the delimiter pattern
 
-        String fileName = "media_collection.csv";
+            sc.next();             // Skips first line
+            
+            String crewId;
+            String primaryName;
+            String birthYear;
+            String[] splitLine;
 
-        try {
-            System.out.println("Populating MediaCollection Table...");
-            Statement stmt = conn.createStatement();
-            String sqlString = "";
-            stmt.executeUpdate(sqlString);
+            // Iterate through each line of file
+            while (sc.hasNext()) { 
+                splitLine = (sc.next()).split(",");    // Split line at commas, splitLine is size 3
+                
+                if (splitLine.length < 3) {             // If the array is less than 3 elements
+                    continue;
+                }
+                if (splitLine[2].length() < 4) {       // If the line does not have a birthYear, skip that person
+                    continue;
+                }
 
+                crewId = splitLine[0];
+                primaryName = splitLine[1];
+                birthYear = splitLine[2];
+                birthYear = birthYear.substring(0, birthYear.length()-1);
+                    
+
+                // If the name is super weird, then we're going to skip it
+                if ( !(primaryName.matches("[a-zA-Z\s+_.-]+")) ){
+                    continue;
+                }else{
+                    // Populate database
+                    String sqlCommand = "INSERT INTO crewmembers VALUES('" + crewId + "', '" + primaryName + "', '" + birthYear + "');";
+                    Statement stmt = conn.createStatement();
+                    stmt.executeUpdate(sqlCommand);
+                }
+            }
+
+            // Close the scanner
+            sc.close();
         } catch (Exception e) {
-            System.out.println("Failed to populate MediaCollection table");
-            e.printStackTrace();
-            System.err.println(e.getClass().getName() + ": " + e.getMessage());
-            System.exit(0);
-        }
+            System.out.println("Failed to populate CrewMembers table");
+			e.printStackTrace();
+			System.err.println(e.getClass().getName() + ": " + e.getMessage());
+			System.exit(0);
+		} 
     }
 
     // *Need to iterate a primary key
     public static void scanMediaCrewMembers(Connection conn) {
 
-        String fileName = "media_crew_members.csv";
-
-        try {
-            System.out.println("Populating MediaCrewMembers Table...");
-            Statement stmt = conn.createStatement();
-            String sqlString = "";
-            stmt.executeUpdate(sqlString);
-
-        } catch (Exception e) {
-            System.out.println("Failed to populate MediaCrewMembers table.");
-            e.printStackTrace();
-            System.err.println(e.getClass().getName() + ": " + e.getMessage());
-            System.exit(0);
-        }
     }
 
     // *Need to iterate a primary key
     public static void scanMediaGenres(Connection conn) {
         
-        String fileName = "media_genres.csv";
-        
-        try {
-            System.out.println("Populating MediaGenres Table...");
-            Statement stmt = conn.createStatement();
-            String sqlString = "";
-            stmt.executeUpdate(sqlString);
-
-        } catch (Exception e) {
-            System.out.println("Failed to populate MediaGenres table.");
-            e.printStackTrace();
-            System.err.println(e.getClass().getName() + ": " + e.getMessage());
-            System.exit(0);
-        }
-        // string s;
-        // String[] arrays = s.split(",", 2);
     }
 
     /*-----------------------------------------------------------------------------------------------------*/
