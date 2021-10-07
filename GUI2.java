f232232c94c308d24ccd5e71d3fe682edd7207d1
Daniel <danielchamorro.q@gmail.com>
2021-10-07 10:46:47 -0500

    second push

--- GUI2.java
+++ GUI2.java
@@ -8,25 +8,13 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
-public class GUI2 extends JFrame {
-    ArrayList<Person> aLP = new ArrayList<Person>();
-
+public class GUI2 extends JFrame{
     // use setVisible and setText for buttons
-
-    // left list of contacts
-
-    // alphabetical
-    JList pplList = new JList(aLP.toArray());
 
     // First screen, prompt the user to enter their customerID
     JLabel lbl_customerID = new JLabel("Customer ID: ");
     JTextField txt_customerID = new JTextField();
-
-    // instruction label
     JLabel lbl_instruction = new JLabel("Enter your customer ID");
-
-
-    // bottom two buttons when creating contact
     JButton btn_save = new JButton("Enter");
 
     public GUI2() {
@@ -44,24 +32,13 @@ public class GUI2 extends JFrame {
 
         // left list of contacts
 
-        // create a scroller, set its size, set scroller in constructor give it list
+        // Create a scroller, set its size, set scroller in constructor give it list
         lbl_instruction.setBounds(100, 10, 210, 25);
 
         // First screen, prompt the user to enter their customerID
         lbl_customerID.setBounds(100, 40, 175, 25);
         txt_customerID.setBounds(190, 40, 160, 25);
-
-        // instructions
-
-        //x, y, width, height
-
-        // 2 buttons
-        btn_save.setBounds(250, 300, 160, 25);
-
-        // 2 buttons when contact is selected
         btn_save.setBounds(150, 250, 100, 25);
-
-        // change away
 
         // ------------------------------------------------------------------------------------------------
         // running the operation once the button is pressed
@@ -89,11 +66,6 @@ public class GUI2 extends JFrame {
 
         // buttons
         add(btn_save);
-        // add(btn_new);
-
-        // buttons for editing a contact
-
-        // change away
 
         // --------------------------------------------------------------------------------------------------
         setVisible(true);
@@ -101,76 +73,6 @@ public class GUI2 extends JFrame {
     }
 
     boolean needsMore = false;
-
-    // JoptionPane.showConfirmDialaog(null, "Overrite previous message", "");
-
-    // public void save() {
-    //     // System.out.println("customerID: "+txt_customerID.getText().equals(""));
-    //     // make a new instance of a person each time instead of trying to change the
-    //     // previous
-
-    //     if (txt_customerID.getText().equals("") || txt_lastName.getText().equals("")) {
-    //         needsMore = true;
-    //         lbl_instruction.setText("First and Last Name are Required");
-    //     }
-    //     // else
-    //     // {
-    //     // lbl_instruction.setText("Enter your contact information");
-    //     // }
-
-    //     try {
-    //         // System.out.println(needsMore);
-    //         if (!needsMore) {
-    //             Person p = new Person("John", "Doe", "0", "");
-
-    //             p.setfName(txt_customerID.getText());
-
-    //             // collections.sort every time you add to the list or change it
-    //             // ADD TO ARRAYLIST
-
-    //             // System.out.println("aLP: " + aLP);
-
-    //             // System.out.println("p.toString(): " + p.toString());
-    //             // SORT Arraylist ALPHABETICALLY
-    //             // String.CASE_INSENSITIVE_ORDER
-
-    //             /*
-    //              * if(aLP.size() > 1){ String contactName1, contactName2;
-    //              * 
-    //              * for (int i = 0; i < aLP.size ( ); i++ ) { contactName1 = aLP.get(i).getlName(
-    //              * ); for (int n = i + 1; n < aLP.size ( )-1; n++ ) { contactName2 =
-    //              * aLP.get(n).getlName( );
-    //              * 
-    //              * if (contactName1.compareToIgnoreCase (contactName2) > 0) {
-    //              * System.out.println("con1 is greater than con2"); Person temp = aLP.get (i);
-    //              * aLP.set (i, aLP.get (n)); aLP.set (n, temp);
-    //              * 
-    //              * System.out.println("\ntemp " + temp.getlName( ) + "\n contact 1 " + aLP.get
-    //              * (i).getlName ( ) + "\n contact 2 " + aLP.get (n).getlName ( )); } } } }else {
-    //              * aLP.add(p); }
-    //              */
-
-    //             // aLP.add(p);
-
-    //             /*
-    //              * for(int i = 0; i<aLP.size(); i++) { if(comparing(aLP.get(1), aLP.get(2)) > 1)
-    //              * { aLP.add( i, p); } else{ aLP.add(p); } }
-    //              */
-
-    //             // Collections.sort(aLP.toString());
-
-    //             aLP.add(p);
-    //             // add to list
-    //             pplList.setListData(aLP.toArray());
-
-    //             txt_customerID.setText("");
-    //         }
-
-    //     } catch (Exception l) {
-    //         // clear();
-    //         lbl_instruction.setText("Invalid Input");
-    //     }
-    // }
 
     //* All methods deleted
 
--- MainFile.java
+++ MainFile.java
@@ -1,6 +1,35 @@
 public class MainFile {
     public static void main(String[] args)
     {
+        Building the connection
+        Connection conn = null;
+        // TODO STEP 1
+        try {
+            Class.forName("org.postgresql.Driver");
+            conn = DriverManager.getConnection("jdbc:postgresql://csce-315-db.engr.tamu.edu/csce315903_11db",
+                    "csce315903_11user", "new_password");
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+        JOptionPane.showMessageDialog(null, "Opened database successfully");
+
+        String name = "";
+        try {
+            // create a statement object
+            Statement stmt = conn.createStatement();
+            // create an SQL statement
+            // TODO Step 2
+            String sqlStatement = "SELECT * FROM mediacollection LIMIT 10;";
+            // send statement to DBMS
+            ResultSet result = stmt.executeQuery(sqlStatement);
+            while (result.next()) {
+                name += result.getString("media_title") + "\n";
+            }
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Error accessing Database.");
+        }
         new GUI2();
     }
 }
