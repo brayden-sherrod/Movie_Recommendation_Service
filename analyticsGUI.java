671d16e5cab18bed979093f7215c83b5dfd38d66
Rose Soriano <rosesoriano.dtx@gmail.com>
2021-10-10 10:29:56 -0500

    Corrected implementation error

--- analyticsGUI.java
+++ analyticsGUI.java
@@ -5,7 +5,7 @@ import java.awt.BorderLayout;
 import java.awt.GridLayout;
 import java.awt.*;   
 
-public class analyticsGUI extends JFrame implements ActionListener {
+public class analyticsGUI extends JFrame {
   
     JPanel panel = new JPanel();
 
