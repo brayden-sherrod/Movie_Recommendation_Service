f741cf8608095f8621dedca61da391718c0502ba
Rose Soriano <laurensori@tamu.edu>
2021-10-11 15:06:39 -0500

    Had to change our design to be able to enter a start and end date in lab

--- analyticsGUI.java
+++ analyticsGUI.java
@@ -17,6 +17,9 @@ public class analyticsGUI extends JFrame {
 
     JButton btn_back_to_welc = new JButton("Back to Welcome Page");
 
+    JTextField startField = new JTextField();
+    JTextField endField = new JTextField();
+
     public analyticsGUI() {
         // Frame configurations
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -30,6 +33,10 @@ public class analyticsGUI extends JFrame {
 
         // Components
         JLabel titleText = new JLabel("Top 10 Most Watched Media");
+
+        // Components config
+        startField.setEditable(true);
+        endField.setEditable(true);
         
         // Back to welcome page button
         btn_back_to_welc.setBounds(10, 10, 200, 25);
@@ -44,24 +51,64 @@ public class analyticsGUI extends JFrame {
         JButton yearly = new JButton("yearly");
         yearly.addActionListener(e -> viewYearly());
 
+        JButton enterButton = new JButton("Enter");
+        enterButton.addActionListener(e -> enterInfo());
+
         // Configure component placements
         titleText.setBounds(270, 20, 200, 30);
         scroll_pane_title_list.setBounds(100, 60, 500, 300);
-        alltime.setBounds(100, 380, 120, 40);
-        monthly.setBounds(290, 380, 120, 40);
-        yearly.setBounds(480, 380, 120, 40);
+        // alltime.setBounds(100, 380, 120, 40);
+        // monthly.setBounds(290, 380, 120, 40);
+        // yearly.setBounds(480, 380, 120, 40);
+        startField.setBounds(100, 380, 120, 40);
+        endField.setBounds(290, 380, 120, 40);
+        enterButton.setBounds(480, 380, 120, 40);
+        
 
         // Add components to frame
         panel.add(titleText);
         panel.add(scroll_pane_title_list);
-        panel.add(alltime);
-        panel.add(monthly);
-        panel.add(yearly);
+        // panel.add(alltime);
+        // panel.add(monthly);
+        // panel.add(yearly);
+        panel.add(startField);
+        panel.add(endField);
+        panel.add(enterButton);
 
         // Add panel to frame and allow visibility
         add(panel, BorderLayout.CENTER);
         setLocationRelativeTo(null); // center the window on the screen
         setVisible(true);
+
+    
+    }
+
+    public void enterInfo(){
+
+        foundTitles.clear();
+        
+        String start_date = startField.getText();
+        String end_date = endField.getText();
+
+        MainFile mainFile = new MainFile();
+
+        // Connect to database
+        ResultSet rs = mainFile.runSQLString("SELECT A.media_title FROM mediacollection A WHERE A.media_id in (SELECT media_id FROM (SELECT media_id,COUNT(media_id) AS value_occurrence FROM customersratings WHERE (date_rated BETWEEN '" + start_date + "' AND '" + end_date + "') GROUP BY media_id ORDER BY value_occurrence DESC LIMIT 10) AS foo);");
+        try {
+            while (rs.next()) {
+                foundTitles.add(rs.getString("media_title") + "\n");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        
+        // Display the titles
+        foundTitlesList.setListData(foundTitles.toArray());
+        foundTitlesList.repaint();
+        scroll_pane_title_list.repaint();
+        for (int i = 0; i < foundTitles.size(); i++) {
+            System.out.println(foundTitles.get(i));
+        }
     }
 
     public void backWelcFunc(){
@@ -105,8 +152,10 @@ public class analyticsGUI extends JFrame {
         foundTitles.clear();
         MainFile mainFile = new MainFile();
 
+        String start_date = "2005-11-30";
+        String end_date = "2005-12-21";
         // Connect to database
-        ResultSet rs = mainFile.runSQLString("SELECT A.media_title FROM mediacollection A WHERE A.media_id in (SELECT media_id FROM (SELECT media_id,COUNT(media_id) AS value_occurrence FROM customersratings WHERE (date_rated BETWEEN '2005-11-30' AND '2005-12-21') GROUP BY media_id ORDER BY value_occurrence DESC LIMIT 10) AS foo);");
+        ResultSet rs = mainFile.runSQLString("SELECT A.media_title FROM mediacollection A WHERE A.media_id in (SELECT media_id FROM (SELECT media_id,COUNT(media_id) AS value_occurrence FROM customersratings WHERE (date_rated BETWEEN '" + start_date + "' AND '" + end_date + "') GROUP BY media_id ORDER BY value_occurrence DESC LIMIT 10) AS foo);");
         try {
             while (rs.next()) {
                 foundTitles.add(rs.getString("media_title") + "\n");
